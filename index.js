8a6fc195ef4b63225aa2109efc72e8fa1a422d3a
Uoc Developer <39707744-uocdev@users.noreply.replit.com>
2025-06-18 04:20:22 +0000

    Remove old module.exports package

--- index.js
+++ index.js
@@ -1,10 +1,2 @@
-const { getDiscordTimestamp, getISOTime, getUnix, now } = require('./timestamp/formats.js');
-const { init } = require('./timestamp/init.js')
 
-module.exports = {
-  discord: getDiscordTimestamp,
-  iso: getISOTime,
-  unix: getUnix,
-  now
-};
 // require some others packages timestamp for good results,  this is still wip but thank you for using my package :D
