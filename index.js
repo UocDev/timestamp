5e36eddf66116f007848bebef082fa09b4d45299
Uoc Developer <39707744-uocdev@users.noreply.replit.com>
2025-06-18 04:30:50 +0000

    update index file

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const now = require('./src/timestamp/now');
+const unix = require('./src/timestamp/unix');
+const discordFull = require('./src/timestamp/discordFull');
+const htmlTime = require('./src/timestamp/htmlTime');
+const androidISO = require('./src/timestamp/androidISO');
 
+module.exports = {
+  now,
+  unix,
+  discordFull,
+  htmlTime,
+  androidISO
+};
 // require some others packages timestamp for good results,  this is still wip but thank you for using my package :D
