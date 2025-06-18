583197695730898953f038df3644c902d82e3cb8
Uoc Developer <39707744-uocdev@users.noreply.replit.com>
2025-06-18 05:43:02 +0000

    update index can support new modules

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ const discordFull = require('./src/timestamp/discordFull');
 const htmlTime = require('./src/timestamp/htmlTime');
 const androidISO = require('./src/timestamp/androidISO');
 
+const setConfig = require('./src/config/setConfig');
+const getConfig = require('./src/config/getConfig');
+const ensureConfigDir = require('./src/config/ensureConfigDir')
+
 module.exports = {
   now,
   unix,
   discordFull,
   htmlTime,
-  androidISO
+  androidISO,
+  setConfig,
+  getConfig,
+  ensureConfigDir
 };
 // require some others packages timestamp for good results,  this is still wip but thank you for using my package :D
