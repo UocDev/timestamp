91b723dcda2237bb1d0d9935df3ad85a917f05c2
Uoc Developer <39707744-uocdev@users.noreply.replit.com>
2025-06-18 05:38:29 +0000

    update now() can support config Module

--- src/timestamp/now.js
+++ src/timestamp/now.js
@@ -1,4 +1,11 @@
-function now(locale = 'id-ID') {
-  return new Date().toLocaleString(locale);
+const ensureConfigDir = require('./config/ensureConfigDir');
+const getConfig = require('./config/getConfig');
+
+function now(locale) {
+  ensureConfigDir();
+  const config = getConfig();
+  const selectedLocale = locale || config.locale || 'id-ID';
+  return new Date().toLocaleString(selectedLocale);
 }
+
 module.exports = now;
