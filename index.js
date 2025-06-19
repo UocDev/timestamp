41949ddfa856e595275949c5918108779a73ed72
Uoc Developer <39707744-uocdev@users.noreply.replit.com>
2025-06-19 05:03:59 +0000

    update module root in index

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const discordFull = require('./src/timestamp/discordFull');
 const htmlTime = require('./src/timestamp/htmlTime');
 const androidISO = require('./src/timestamp/androidISO');
 const saveTimestamp = require('./src/timestamp/saveTimestamp')
+const base64Encode = require('./src/timestamp/base64Encode');
+const base64Decode = require('./src/timestamp/base64Decode');
+const utfEncode = require('./src/timestamp/utfEncode');
+const utfDecode = require('./src/timestamp/utfDecode');
 
 module.exports = {
   now,
@@ -11,7 +15,10 @@ module.exports = {
   discordFull,
   htmlTime,
   androidISO,
-  saveTimestamp
-  
+  saveTimestamp, 
+  base64Encode,
+  base64Decode,
+  utfEncode,
+  utfDecode
 };
 // require some others packages timestamp for good results,  this is still wip but thank you for using my package :D
