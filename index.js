ac04dab81976dbfe2db05ee6e73b03818f7bd743
Uoc Developer <39707744-uocdev@users.noreply.replit.com>
2025-06-20 09:18:58 +0000

    fix bug

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ module.exports = {
   syncTimeFrom,
   syncTime,
   syncAndCorrect,
-  autoSync
+  autoSync,
 };
 // require some others packages timestamp for good results,  this is still wip but thank you for using my package :D
