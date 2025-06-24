f04b0b00c2a1183d9a40d556e120cd42f553b241
Uoc Developer <uocdev.github.io@gmail.com>
2025-06-25 06:04:25 +0800

    Update index.js warning message when executing scripts without higher version Node

--- dist/index.js
+++ dist/index.js
@@ -2,7 +2,7 @@ const [major] = process.versions.node.split('.').map(Number);
 
 if (major < 18 || major >= 26) {
   console.error(`Error RPM Node.js version: ${process.version}`);
-  console.error(`Required Node.js 18.x or higher. We make this system for safety debugger bugs unknown if use in older versions. Please download new Node or install higher Node :3`);
+  console.error(`Required Node.js 18.x or higher. Pleaseee download new LTS or currently version Node :3`);
   process.exit(1);
 }
 const now = require('../src/timestamp/now');
